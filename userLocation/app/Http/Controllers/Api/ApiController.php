0909b9d511d2af557782c4dcbd775912f6deab1e
sheyls <sheylvsz01@gmail.com>
2024-05-09 03:59:56 -0400

    Create login, perfil, logout

--- userLocation/app/Http/Controllers/Api/ApiController.php
+++ userLocation/app/Http/Controllers/Api/ApiController.php
@@ -44,15 +44,80 @@ class ApiController extends Controller
                 'message' => 'User created successfull',
                 'token' => $user->createToken("API TOKEN")->plainTextToken
             ],200);
-    } catch(\Throwable $th){
-        return response()->json([
-            'status' => false,
-            'message' => $th->getMessage(),
 
-        ],500);
+        } catch(\Throwable $th){
+
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+
+            ],500);
+        }
+
     }
 
+    public function login(Request $request)
+    {   
+        try{
+            $validateUser = Validator::make($request->all(),
+            [
+                'email' => 'required|email',
+                'password' => 'required',
+
+            ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message'=> 'Validation error',
+                    'errors' => $validateUser->errors()
+                ],401);
+            }
+
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'status' => false,
+                    'message'=> 'Email and password does incorrects',
+        
+                ],401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User logged in successfull',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ],200);
+
+
+
+        } catch(\Throwable $th){
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+
+            ],500);
+        }
+
+    }
+
+    public function profile(){
+        $userData = auth()->user();
+        return response()->json([
+            'status' => true,
+            'message' => 'This is Profile',
+            'data' => $userData,
+            'id' => auth()->user()->id
+        ],200);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'User logged out',
+            'data' => []
+        ],200);
     }
 }
-
-// Register, Login, Profile and logout
--- userLocation/routes/api.php
+++ userLocation/routes/api.php
@@ -4,10 +4,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
 
-/*Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');*/
-
 
 //Register
 Route::post("register", [ApiController::class, "register"]);
+
+//Login
+Route::post("login", [ApiController::class, "login"]);
+
+Route::group([
+    "middleware" => ('auth:sanctum')
+], function(){
+
+    //Profile
+    Route::post("profile", [ApiController::class, "profile"]);
+
+    //Logout
+    Route::post("logout", [ApiController::class, "logout"]);
+
+});
+
+/*Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');*/
